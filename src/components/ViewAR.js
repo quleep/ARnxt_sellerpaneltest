26324df1b941936174b52cf57cfa9d104c04ffcf
Omanshuamanarnxt <omanshuaman@email.com>
2023-10-21 12:15:10 +0530

    view in room home page

--- src/components/ViewAR.js
+++ src/components/ViewAR.js
@@ -1,421 +1,241 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './Navbar'
-import Footertest from './Footertest'
-import axios from 'axios';
-import ReactPaginate from 'react-paginate';
-import { useHistory, useLocation } from 'react-router-dom';
-import { FaArrowLeft, FaArrowRight, FaGreaterThan } from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import Footertest from "./Footertest";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
+import { useHistory, useLocation } from "react-router-dom";
+import { FaArrowLeft, FaArrowRight, FaGreaterThan } from "react-icons/fa";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const ViewAR = () => {
-    const productdetailsurl= 'https://ymxx21tb7l.execute-api.ap-south-1.amazonaws.com/production/getproducttable'
-    const categoryurl = 'https://ymxx21tb7l.execute-api.ap-south-1.amazonaws.com/production/getcategorydetails'
-    const fetchsubcat = 'https://ymxx21tb7l.execute-api.ap-south-1.amazonaws.com/production/getsubcategoryitems'
-    const fetchtagdata = 'https://ymxx21tb7l.execute-api.ap-south-1.amazonaws.com/production/gettags'
-    const [allproducts, setAllProducts] = useState()
-    const [offset, setOffset] = useState(0);
-    const [data, setData] = useState([]);
-    const [perPage] = useState(10);
-    const [pageCount, setPageCount] = useState(0)
-    const [offsetvalue, setOffsetValue] = useState();
-    const [activepagevalue, setActivePagevalue] = useState(0)
-    const [categorydata, setCategoryData] = useState()
-    const [subcatvalue, setSubCatValue] = useState('')
-    const [subcategoryitems, setSubCategoryItems] = useState()
+  const [brandsData, setBrandsData] = useState([]);
+  const [brandRooms, setBrandRooms] = useState(null);
+  const [categoriesDetails, setCategoriesDetails] = useState([]);
+  const [roomData, setRoomData] = useState([]);
 
-    const location = useLocation()
+  useEffect(() => {
+    // Fetch data using Axios
+    axios
+      .get(
+        "https://3ef9gn5kk2.execute-api.ap-south-1.amazonaws.com/arnxt_prod/rooms"
+      )
+      .then((response) => {
+        const shuffledData = shuffleArray(response.data.data);
 
-    useEffect(()=>{
-      if(location.state && location.state.arviewreturn === 'true' && location.state.offsetvalue > 10){
-         setActivePagevalue(location.state.offsetvalue/10)
-        setOffset(location.state.offsetvalue)
-      }else{
-        setOffset(0)
-        setActivePagevalue(0)
-
-      }
-
-    },[])
-
-    useEffect(()=>{
-     let newarr =[]
-      axios.get(categoryurl).then(res=>{
-        console.log(res)
-      
-         for (let i =0 ; i< res.data.length ; i++){
-          if (res.data [i].category === 'Furniture'){
-            newarr[0] = res.data[i]
-          }
-          if (res.data [i].category === 'Bathroom'){
-            newarr[1] = res.data[i]
-          }
-          if (res.data [i].category === 'Furnishing'){
-            newarr[2] = res.data[i]
-          }
-          if (res.data [i].category === 'Electrical'){
-            newarr[3] = res.data[i]
-          }
-          if (res.data [i].category === 'Electronics'){
-            newarr[4] = res.data[i]
-          }
-          if (res.data [i].category === 'Decorative'){
-            newarr[5] = res.data[i]
-          }
-          if (res.data [i].category === 'Walls'){
-            newarr[6] = res.data[i]
-          }
-          if (res.data [i].category === 'Floors'){
-            newarr[7] = res.data[i]
-          }
-         }
-         console.log(newarr)
-        setCategoryData(newarr)
-         
-      }).catch(error=>{
-        console.log(error)
+        setRoomData(shuffledData);
       })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+  const fetchCategoriesData = async () => {
+    try {
+      const response = await axios.get(
+        "https://ymxx21tb7l.execute-api.ap-south-1.amazonaws.com/production/getcategorydetails"
+      );
+      console.log("categ", response.data);
 
-    },[])
+      const shuffledData = shuffleArray(response.data);
 
-   
-      
-   const history = useHistory()
-    useEffect(()=>{
-      axios.get(productdetailsurl).then(res=>{
-        setAllProducts(res.data)
-      
-        setPageCount(Math.ceil(res.data.length / perPage))
-       
-      }).catch(error=>{
-        console.log(error)
-      })  
-
-    },[])
- 
-
-
-    const slice = allproducts && allproducts.slice(offset, offset + perPage)
-
-  
-
-    
-    const handlePageClick=(e)=>{
-      window.scrollTo(0,0)
-        
-        const selectedPage = e.selected;
-        
-        setOffsetValue(e.selected)
-        setOffset(selectedPage*10)
-
-       
+      setCategoriesDetails(shuffledData);
+    } catch (error) {
+      console.error("Error in fetching data: ", error);
+      // Handle the error
     }
-
-   
-    const handleDetails=(item)=>{
-
-        if(item.modelrequired === 'true'){
-            history.push({
-                pathname: '/details',
-                state: {
-                  id: item.product_Id,
-                  offsetvalue: offsetvalue*10
-                }
-            })
-        }
-      if(item.modelrequired === 'false'){
-        history.push({
-            pathname: '/webdetails',
-            state: {
-              id: item,
-              offsetvalue: offsetvalue*10
-            }
-        })
-      }
+  };
+  // Function to shuffle an array randomly
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
     }
+    return shuffledArray;
+  };
+  const brands = async () => {
+    try {
+      const response = await axios.get(
+        "https://ymxx21tb7l.execute-api.ap-south-1.amazonaws.com/production/getbrandtable"
+      );
+      const response1 = await axios.get(
+        "https://ymxx21tb7l.execute-api.ap-south-1.amazonaws.com/production/getallbrands"
+      );
 
-   const handlemodalclick =()=>{
-    document.querySelector('.modalnew').classList.toggle('modalnewclick')
-    
-   }
-   const handleclosemodal = ()=>{
-   
+      console.log("brandsarray", response);
+      const brandFilter = response1.data; // Array of brand-ids to filter by (all lowercase)
 
-  document.querySelector('.modalnew').classList.remove('modalnewclick')
+      const filteredData = response.data.filter((item) =>
+        brandFilter.includes(item["brandId"].toLowerCase())
+      );
+      const shuffledData = shuffleArray(filteredData);
+      setBrandsData(shuffledData);
 
- 
-   }
-   let tempsub;
-   const handlecategoryclick = (val)=>{
-    
-      setSubCatValue(val)
-      document.querySelector('.subcategorydiv').style.display = 'block'
-      document.querySelector('.tagsdiv').style.display = 'none'
-      document.querySelector('.categorydiv').style.display = 'none'
-
-   }
-
-   const handlegoback =(e)=>{
-    e.preventDefault()
-    document.querySelector('.subcategorydiv').style.display = 'none'
-    document.querySelector('.tagsdiv').style.display = 'block'
-    document.querySelector('.categorydiv').style.display = 'block'
-   }
-
-   const handlesubcatclick =(val)=>{
-     const body= {
-       subcategory: val
-     }
-     axios.post(fetchsubcat, body).then(res=>{
-       if(res.data.length === 0){
-        window.scrollTo(0,0)
-        document.querySelector('.nodatapopup').style.display = 'flex'
-       }
-       if(res.data.length > 0){
-        document.querySelector('.nodatapopup').style.display = 'none'
-
-       }
-       setOffset(0)
-       document.querySelector('.modalnew').classList.remove('modalnewclick')
-       setAllProducts(res.data)
-       setPageCount(Math.ceil(res.data.length / perPage))
-        }).catch(error=>{
-      console.log(error)
-     })
-   }
-
-   const handletagsdata = ()=>{
-      const body = {
-        tags : "New"
-      }
-      axios.post(fetchtagdata, body).then(res=>{
-          if(res.data.length > 0){
-            window.scrollTo(0,0)
-        document.querySelector('.nodatapopup').style.display = 'none'
-
-          }
-       setOffset(0)
-          
-          setAllProducts(res.data)
-  document.querySelector('.modalnew').classList.remove('modalnewclick')
-
-          setPageCount(Math.ceil(res.data.length / perPage))
-  
-
-      }).catch(error=>{
-        console.log(error)
-      })
-   }
-
-   const handletagsdatalatest = ()=>{
-    const body = {
-      tags : "Top Picks"
+      console.log(filteredData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    axios.post(fetchtagdata, body).then(res=>{
-      if(res.data.length > 0){
-        window.scrollTo(0,0)
-        document.querySelector('.nodatapopup').style.display = 'none'
+  };
+  useEffect(() => {
+    brands();
 
-          }
-       setOffset(0)
+    fetchCategoriesData();
+  }, []); // Empty dependency array ensures this runs only once after initial render
+  useEffect(() => {
+    // Transform the data
+    const transformedData = brandsData?.map((item) => {
+      // Store brandId in lowercase
+      const brandIdLowerCase = item["brand-id"].toLowerCase();
 
-       setAllProducts(res.data)
-  document.querySelector('.modalnew').classList.remove('modalnewclick')
+      // Store iconUrl in a new variable
+      const iconUrlNewVariable = item["iconUrl"];
 
-       setPageCount(Math.ceil(res.data.length / perPage))
-    
+      // Create a new object with the transformed values
+      return {
+        brandId: brandIdLowerCase,
+        iconUrlNewVariable: iconUrlNewVariable,
+      };
+    });
 
-    }).catch(error=>{
-      console.log(error)
-    })
- }
+    // Update the original data with the transformed data
+    setBrandRooms(transformedData);
+    // Display the updated JSON data
+  }, [brandsData]);
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 400;
+  };
 
- const handletagsdatafeatured = ()=>{
-  const body = {
-    tags : "Featured"
-  }
-  axios.post(fetchtagdata, body).then(res=>{
-    if(res.data.length > 0){
-      window.scrollTo(0,0)
-      document.querySelector('.nodatapopup').style.display = 'none'
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 400;
+  };
+  const slideLeft1 = () => {
+    var slider = document.getElementById("slider1");
+    slider.scrollLeft = slider.scrollLeft - 400;
+  };
 
-        }
-       setOffset(0)
+  const slideRight1 = () => {
+    var slider = document.getElementById("slider1");
+    slider.scrollLeft = slider.scrollLeft + 400;
+  };
+  const slideLeft2 = () => {
+    var slider = document.getElementById("slider2");
+    slider.scrollLeft = slider.scrollLeft - 400;
+  };
 
-     setAllProducts(res.data)
-  document.querySelector('.modalnew').classList.remove('modalnewclick')
-
-     setPageCount(Math.ceil(res.data.length / perPage))
-   
-
-  }).catch(error=>{
-    console.log(error)
-  })
-}
-const handlebackproduct =()=>{
-  document.querySelector('.modalnew').classList.add('modalnewclick')
-  document.querySelector('.subcategorydiv').style.display = 'block'
-  document.querySelector('.tagsdiv').style.display = 'none'
-  document.querySelector('.categorydiv').style.display = 'none'
-
-}
-
-  
+  const slideRight2 = () => {
+    var slider = document.getElementById("slider2");
+    slider.scrollLeft = slider.scrollLeft + 400;
+  };
   return (
-    <div>
-       <Navbar/>
-
-       
-       <div className='modelbuttoncontainer'>
-       <div className='hamfilterbutton'  onClick={handlemodalclick}>
-        <span className='linesfilter'></span>
-        <span className='linesfilter'></span>
-        <span className='linesfilter'></span>
-
-         
+    <>
+      <Navbar />
+      <div className="hero_container">
+        <div className="bold_text_container">
+          <div className="bold_text">Rooms</div>
         </div>
-          
-
-       <p id="openModalButton "> All</p>
-
-    
-
-        {
-          /*
-         <div className='topcategorydiv'>
-        {
-          categorydata && categorydata.map(item=>(
-            <div className='topcategoryitems'>
-              <p>
-                {item.category}
-              </p>
+        <div className="hori_scroll_container">
+          <MdChevronLeft
+            className="hori_scroll_left_arrow"
+            onClick={slideLeft}
+            size={40}
+          />
+          <div id="slider" className="hori_scroll_container_child">
+            {roomData.map((item) => (
+              <div
+                key={item.id}
+                className="hori_scroll_container_child1"
+                onClick={() => router.push(`/rooms/${item.roomname}`)}>
+                <img
+                  src={item.iconurl}
+                  alt="/"
+                  className="hori_scroll_container_child1_image"
+                />
+                {/* Add the text here */}
+                <p className="hori_scroll_container_child1_text">
+                  {item.roomname.charAt(0).toUpperCase() +
+                    item.roomname.slice(1)}
+                </p>
               </div>
-          ))
-        }
-        
-
-
-     </div>
-          */
-        }
- 
-
-      
-       </div>
-       <div className='nodatapopup'>
-        <div className='popupdata'>
-          <p>Remodelling to perfection</p>
-          <button  onClick={handlebackproduct} >Go back</button>
+            ))}
+          </div>
+          <MdChevronRight
+            className="hori_scroll_left_arrow"
+            onClick={slideRight}
+            size={40}
+          />
         </div>
-       </div>
-      
-    <div id="modal" class="modalnew">
-        <div class="modal-content">
-            <span class="close" id="closeModalButton" onClick={handleclosemodal}>&times;</span>
-            <div className='modaldata'>
-              <div className='tagsdiv'>
-                <h2>Trending</h2>
-                 <ul>
-                  <li onClick={handletagsdata}>
-                    <p>New</p>
-                  </li>
-                  <li>
-                    <p onClick={handletagsdatalatest} >Top Picks</p>
-                  </li>
-                  <li onClick={handletagsdatafeatured}>
-                    <p>Featured</p>
-                  </li>
-                 </ul>
-
-              </div>
-              <div className='categorydiv'>
-              <h2> Category Items</h2>
-                 <ul>
-                  {
-                    categorydata && categorydata.map((item,index) =>(
-
-                          
-                      <li  onClick={()=>handlecategoryclick(item.category)}>
-                       <p>{item.category}  <FaGreaterThan style={{float:'right', margin:'5px'}} /> </p> 
-                      
-                      </li>
-                    ))
-                  }
-                 
-               
-                 </ul>
-              </div>
-              <div className='subcategorydiv'>
-
-                <button  type='submit' onClick={handlegoback} > <FaArrowLeft/> Go back</button>
-              <h2>Subcategory Items</h2>
-                 <ul>
-                   {
-                    categorydata && categorydata.map(item=>(
-                      item.category === subcatvalue ?
-                         item.subcategory.map(itemnew=>(
-                          <li  onClick={()=>handlesubcatclick(itemnew.itemname)} ><p>{itemnew.itemname}</p></li>
-                         )) : ''
-                    ))
-                   }
-                 </ul>
-              </div>
-             
-
-            </div>
+        <div className="bold_text_container">
+          <div className="bold_text">Categories</div>
         </div>
-    </div>
+        <div className="hori_scroll_container">
+          <MdChevronLeft
+            className="hori_scroll_left_arrow"
+            onClick={slideLeft1}
+            size={40}
+          />
+          <div id="slider1" className="hori_scroll_container_child">
+            {categoriesDetails.map((item) => (
+              <div
+                key={item.category}
+                className="hori_scroll_container_child1"
+                onClick={() => router.push(`/categories/${item.category}`)}>
+                <img
+                  src={item.categoryimage}
+                  alt="/"
+                  className="hori_scroll_container_child1_image"
+                />
+                <p className="hori_scroll_container_child1_text">
+                  {item.category.charAt(0).toUpperCase() +
+                    item.category.slice(1)}
+                </p>
+              </div>
+            ))}
+          </div>
 
-        <div className='arviewcontainer'>
-            <div className='arproductscontainer'>
-                {
-                    slice && slice.map(item=>(
-                        <div>
-                        <div className='arproductcard'>
-                               
-                           <img src={item.imageurl && item.imageurl[0]}/>
-    
-                             <button onClick={()=>handleDetails(item)}>View Details</button>
-         
-                           
-    
-                        </div>
-       
-                    </div>
-                    
-
-                    ))
-                    
-                }
-              
-          
-                
-              
-
-
-            </div>
-            <div className='paginationcontainer'>
-            <ReactPaginate
-                    previousLabel={"prev"}
-                    nextLabel={"next"}
-                    breakLabel={"..."}
-                    breakClassName={"break-me"}
-                    forcePage={activepagevalue}
-                    pageCount={pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={handlePageClick}
-                    containerClassName={"paginationar"}
-                    subContainerClassName={"pages pagination"}
-                    activeClassName={"active"}/>
-
-            </div>
-       
-      
-
+          <MdChevronRight
+            className="hori_scroll_left_arrow"
+            onClick={slideRight1}
+            size={40}
+          />
         </div>
-        <Footertest/> 
-      
-    </div>
-  )
-}
+        <div className="bold_text_container">
+          <div className="bold_text">Brands</div>
+        </div>
+        <div className="hori_scroll_container">
+          <MdChevronLeft
+            className="hori_scroll_left_arrow"
+            onClick={slideLeft2}
+            size={40}
+          />
+          <div id="slider2" className="hori_scroll_container_child">
+            {brandsData?.map((item) => (
+              <div
+                key={item.brandId}
+                className="hori_scroll_container_child1"
+                onClick={() => router.push(`/brands/${item.brandId}`)}>
+                <img
+                  src={item.iconUrl}
+                  alt="/"
+                  className="hori_scroll_container_child1_image_brands"
+                />
+              </div>
+            ))}
+          </div>
 
-export default ViewAR
+          <MdChevronRight
+            className="hori_scroll_left_arrow"
+            onClick={slideRight2}
+            size={40}
+          />
+        </div>
+      </div>
+      <Footertest />
+    </>
+  );
+};
+
+export default ViewAR;
