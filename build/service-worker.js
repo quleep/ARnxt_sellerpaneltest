441ceb7b58639c051f887e1dd556eb83a6699076
anil-arnxt <anil.samal@arnxt.com>
2023-11-21 16:46:10 +0530

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8da7140ec31fafc1d39799fbff673aa2.js"
+  "/precache-manifest.d5fe7a5b71b8d4fcef141071b8dcec1d.js"
 );
 
 self.addEventListener('message', (event) => {
