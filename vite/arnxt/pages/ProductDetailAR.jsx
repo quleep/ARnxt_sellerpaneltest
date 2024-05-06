2e6f93a6b4a74dd6a3c2968a5102119de5fec33f
anil-arnxt <anil.samal@arnxt.com>
2024-05-06 11:31:53 +0530

    test build

--- vite/arnxt/pages/ProductDetailAR.jsx
+++ vite/arnxt/pages/ProductDetailAR.jsx
@@ -309,7 +309,7 @@ function ProductDetailAR() {
                         touch-action="pan-y"
                         src={glbFile}
                         ar
-                      ar-placement
+                      ar-placement={placement}
                       
                         alt="A 3D model of a helmet"
                         ref={modelViewerRef}
@@ -354,7 +354,7 @@ function ProductDetailAR() {
                       animation-name="Dance"
                       ar
                       ar-scale="fixed"
-                      ar-placement
+                      ar-placement={placement}
                         ar-modes="webxr scene-viewer quick-look"
                       shadow-intensity="1"
                       src={glbFile}
